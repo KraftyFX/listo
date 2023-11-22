63e2f2f1033902c9b79674ea15a7b26cecc73f12
Rafi Khan <rafikhan@gmail.com>
2023-11-22 08:48:41 -0800

    Adding new date formatter

--- src/media/dateutil.ts
+++ src/media/dateutil.ts
@@ -1,3 +1,17 @@
 export function nowAsSeconds(offset: number) {
     return new Date(new Date().valueOf() - (offset * 1000))
 }
+
+export function formatSeconds(seconds: number) {
+    const date = new Date(0);
+    date.setSeconds(seconds);
+    const format = date.toISOString().substring(11, 19);
+
+    if (format.startsWith('00:00:')) {
+        return format.substring(6);
+    } else if (format.startsWith('00:')) {
+        return format.substring(3);
+    } else {
+        return format;
+    }
+}
