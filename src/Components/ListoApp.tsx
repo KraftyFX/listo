0f366676dd84064a488aeb0565e9faba8eb381e2
Rafi Khan <rafikhan@gmail.com>
2023-11-25 12:19:06 -0800

    Player controls are half working...

--- src/Components/ListoApp.tsx
+++ src/Components/ListoApp.tsx
@@ -1,12 +1,17 @@
 import { observer } from 'mobx-react';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { CameraStore } from '~/media/cameraStore';
 import { DigitalVideoRecorder } from '~/media/digitalvideorecorder';
 import { DvrOptions } from '~/media/dvrconfig';
 import { CameraList } from './CameraList';
+import { LiveStreamCommands, LiveStreamControls } from './LiveStreamControls';
+import { PlaybackCommands, PlaybackControls } from './PlaybackContols';
 import { VideoPlayer } from './VideoPlayer';
 
 export const ListoApp = observer(function ListoApp() {
+    let dvr: DigitalVideoRecorder;
+
+    const [isLive, setIsLive] = useState(true);
     const videoRef = useRef<HTMLVideoElement>(null!);
 
     useEffect(function mount() {
@@ -19,20 +24,51 @@ export const ListoApp = observer(function ListoApp() {
 
             console.log(videoRef.current);
 
-            const dvr = new DigitalVideoRecorder(videoRef.current, options);
+            dvr = new DigitalVideoRecorder(videoRef.current, options);
+            dvr.on('modechange', setIsLive);
 
-            // await dvr.showLiveStreamAndStartRecording();
+            await dvr.showLiveStreamAndStartRecording();
         };
 
         initAsync().catch(console.error);
 
-        return function dismount() {};
+        return function dismount() {
+            dvr.removeAllListeners();
+        };
     }, []);
+
+    const onCommand = (command: LiveStreamCommands | PlaybackCommands) => {
+        switch (command) {
+            case 'rewind':
+                dvr.rewind();
+                break;
+            case 'play':
+                dvr.play();
+                break;
+            case 'pause':
+                dvr.pause();
+                break;
+            case 'fastForward':
+                dvr.fastForward();
+                break;
+            case 'slowForward':
+                dvr.slowForward();
+                break;
+            case 'nextFrame':
+                dvr.nextFrame();
+                break;
+        }
+    };
 
     return (
         <>
             <CameraList onChangeCamera={() => location.reload()} />
             <VideoPlayer ref={videoRef} />
+            {isLive ? (
+                <LiveStreamControls onCommand={onCommand} />
+            ) : (
+                <PlaybackControls onCommand={onCommand} />
+            )}
         </>
     );
 });
