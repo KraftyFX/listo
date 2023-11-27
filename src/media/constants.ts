be83e7a8370bf32369320ab86a9e3736cb5162d0
Rafi Khan <rafikhan@gmail.com>
2023-11-26 22:26:13 -0800

    Linting constants file

--- src/media/constants.ts
+++ src/media/constants.ts
@@ -1,6 +1,7 @@
-import { DvrOptions, PlaybackOptions, RecordingOptions } from "./dvrconfig";
+import { DvrOptions, PlaybackOptions, RecordingOptions } from './dvrconfig';
 
-export const LOGITECH_BRIO_CAMERA_ID = '94f3aff55f18fcaa8238d5ae2437608852fcdeae132d61a15b94f197cf364acb';
+export const LOGITECH_BRIO_CAMERA_ID =
+    '94f3aff55f18fcaa8238d5ae2437608852fcdeae132d61a15b94f197cf364acb';
 export const BUILT_IN = '5134f09eebf96f0a8bc51de97e5b2bfb78e846b2cb5791c35516010b8350fc18';
 
 export const FRAMES_PER_SECOND = 10;
@@ -10,18 +11,17 @@ export const REFRESH_RATE_IN_MS = 1000 / FRAMES_PER_SECOND;
 export const DEFAULT_RECORDING_OPTIONS: RecordingOptions = {
     source: LOGITECH_BRIO_CAMERA_ID,
     mimeType: 'video/webm',
-    minSegmentSizeInSec : 5,
-    logging: null
-}
+    minSegmentSizeInSec: 5,
+    logging: null,
+};
 
 export const DEFAULT_PLAYBACK_OPTIONS: PlaybackOptions = {
     maxPlaySpeedFactor: 16,
     minPlaySpeedFactor: 1 / 8,
-    logging: null
-}
+    logging: 'info',
+};
 
 export const DEFAULT_DVR_OPTIONS: DvrOptions = {
-    recording : DEFAULT_RECORDING_OPTIONS,
-    playback : DEFAULT_PLAYBACK_OPTIONS
-}
-
+    recording: DEFAULT_RECORDING_OPTIONS,
+    playback: DEFAULT_PLAYBACK_OPTIONS,
+};
