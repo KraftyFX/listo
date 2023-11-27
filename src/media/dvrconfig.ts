252281d1a06fc4ad8bb4cda51c861d9525d12120
Rafi Khan <rafikhan@gmail.com>
2023-11-26 22:04:28 -0800

    Linting DVR config

--- src/media/dvrconfig.ts
+++ src/media/dvrconfig.ts
@@ -1,17 +1,17 @@
 export interface DvrOptions {
-    recording : RecordingOptions,
-    playback: PlaybackOptions
+    recording: RecordingOptions;
+    playback: PlaybackOptions;
 }
 
 export interface RecordingOptions {
-    source: 'default' | string,
-    mimeType: string,
-    minSegmentSizeInSec: number,
-    logging: string
+    source: 'default' | string;
+    mimeType: string;
+    minSegmentSizeInSec: number;
+    logging: 'info' | 'log';
 }
 
 export interface PlaybackOptions {
-    maxPlaySpeedFactor: number,
-    minPlaySpeedFactor: number,
-    logging: string
+    maxPlaySpeedFactor: number;
+    minPlaySpeedFactor: number;
+    logging: 'info' | 'log';
 }
