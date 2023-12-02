5070f79c0f7a37a297ac1d858e018834fbda6042
Rafi Khan <rafikhan@gmail.com>
2023-12-02 14:37:18 -0800

    Moving TimelineStore into its own file

--- src/Components/Timeline.tsx
+++ src/Components/Timeline.tsx
@@ -1,8 +1,9 @@
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import { observer } from 'mobx-react';
 import React, { useEffect, useRef } from 'react';
 import { DvrStore } from '~/Components/stores/dvrStore';
+import { Bar, TimelineStore } from './stores/timelineStore';
 
 dayjs.extend(duration);
 
@@ -11,100 +12,17 @@ export interface TimelineProps {
     onSnapToTime: () => void;
 }
 
-interface Bar {
-    startTime: Dayjs;
-    durationInMin: number;
-}
-
-class TimelineHelper {
-    private readonly multiplierToMakeTestingEasier = 60;
-    public readonly markerSizeInMin = 10;
-    private readonly minuteSizeInPx = 10;
-
-    constructor(public readonly dvrStore: DvrStore, public readonly recordings: Bar[]) {}
-
-    private get firstRecording() {
-        return this.recordings[0];
-    }
-
-    private get lastRecording() {
-        return this.recordings[this.recordings.length - 1];
-    }
-
-    get liveRecording() {
-        return {
-            startTime: this.dvrStore.recordingStartTime,
-            durationInMin: dayjs
-                .duration({ seconds: this.dvrStore.duration * this.multiplierToMakeTestingEasier })
-                .asMinutes(),
-        };
-    }
-
-    get currentTime() {
-        return this.dvrStore.recordingStartTime.add(
-            this.dvrStore.currentTime * this.multiplierToMakeTestingEasier,
-            'seconds'
-        );
-    }
-
-    get startOfTimeline() {
-        const { startTime } = this.firstRecording;
-
-        return this.getPrevMarkerStartTime(startTime);
-    }
-
-    get endOfTimeline() {
-        const { startTime, durationInMin } = this.dvrStore.isLive
-            ? this.liveRecording
-            : this.lastRecording;
-
-        const endOfRecording = startTime.add(durationInMin, 'minutes');
-
-        return this.getPrevMarkerStartTime(endOfRecording).add(this.markerSizeInMin, 'minutes');
-    }
-
-    private getPrevMarkerStartTime(time: dayjs.Dayjs) {
-        const minFromPrevMarkerStart =
-            this.markerSizeInMin * Math.floor(time.minute() / this.markerSizeInMin);
-
-        return time.startOf('hour').add(minFromPrevMarkerStart, 'minutes');
-    }
-
-    getTimelineMinutesFromTime(time: dayjs.Dayjs) {
-        const startOfDay = this.firstRecording.startTime.startOf('day');
-        const timelineStartInSec = this.startOfTimeline.diff(startOfDay, 'seconds');
-
-        const seconds = time.diff(startOfDay, 'seconds') - timelineStartInSec;
-        const minutes = seconds / 60;
-
-        return minutes;
-    }
-
-    getAsPixels(timelineMinutes: number) {
-        return timelineMinutes * this.minuteSizeInPx;
-    }
-}
-
 export const Timeline = observer(function Timeline(props: TimelineProps) {
     const { dvrStore } = props;
     const liveBarRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        // recordings.push({
-        //     startTime: recordings[0].startTime.subtract(15, 'minutes'),
-        //     durationInMin: dayjs.duration({ minutes: 10 }).asMinutes(),
-        // });
-
         liveBarRef.current?.scrollIntoView({ block: 'end', inline: 'end' });
     }, []);
 
     const recordings: Bar[] = [];
 
-    const timeline = new TimelineHelper(dvrStore, recordings);
-
-    if (dvrStore.isLive) {
-        recordings.push(timeline.liveRecording);
-    }
+    const timeline = new TimelineStore(dvrStore, recordings);
 
     function getThumb() {
         const thumbTimeInMin = timeline.getTimelineMinutesFromTime(timeline.currentTime);
---
+++ src/Components/stores/timelineStore.ts
@@ -0,0 +1,81 @@
+import dayjs, { Dayjs } from 'dayjs';
+import { DvrStore } from './dvrStore';
+
+export interface Bar {
+    startTime: Dayjs;
+    durationInMin: number;
+}
+
+export class TimelineStore {
+    private readonly multiplierToMakeTestingEasier = 60;
+    public readonly markerSizeInMin = 10;
+    private readonly minuteSizeInPx = 10;
+
+    constructor(public readonly dvrStore: DvrStore, public readonly recordings: Bar[]) {
+        // makeAutoObservable(this);
+        if (dvrStore.isLive) {
+            recordings.push(this.liveRecording);
+        }
+    }
+
+    private get firstRecording() {
+        return this.recordings[0];
+    }
+
+    private get lastRecording() {
+        return this.recordings[this.recordings.length - 1];
+    }
+
+    private get liveRecording() {
+        return {
+            startTime: this.dvrStore.recordingStartTime,
+            durationInMin: dayjs
+                .duration({ seconds: this.dvrStore.duration * this.multiplierToMakeTestingEasier })
+                .asMinutes(),
+        };
+    }
+
+    get currentTime() {
+        return this.dvrStore.recordingStartTime.add(
+            this.dvrStore.currentTime * this.multiplierToMakeTestingEasier,
+            'seconds'
+        );
+    }
+
+    get startOfTimeline() {
+        const { startTime } = this.firstRecording;
+
+        return this.getPrevMarkerStartTime(startTime);
+    }
+
+    get endOfTimeline() {
+        const { startTime, durationInMin } = this.dvrStore.isLive
+            ? this.liveRecording
+            : this.lastRecording;
+
+        const endOfRecording = startTime.add(durationInMin, 'minutes');
+
+        return this.getPrevMarkerStartTime(endOfRecording).add(this.markerSizeInMin, 'minutes');
+    }
+
+    private getPrevMarkerStartTime(time: dayjs.Dayjs) {
+        const minFromPrevMarkerStart =
+            this.markerSizeInMin * Math.floor(time.minute() / this.markerSizeInMin);
+
+        return time.startOf('hour').add(minFromPrevMarkerStart, 'minutes');
+    }
+
+    getTimelineMinutesFromTime(time: dayjs.Dayjs) {
+        const startOfDay = this.firstRecording.startTime.startOf('day');
+        const timelineStartInSec = this.startOfTimeline.diff(startOfDay, 'seconds');
+
+        const seconds = time.diff(startOfDay, 'seconds') - timelineStartInSec;
+        const minutes = seconds / 60;
+
+        return minutes;
+    }
+
+    getAsPixels(timelineMinutes: number) {
+        return timelineMinutes * this.minuteSizeInPx;
+    }
+}
