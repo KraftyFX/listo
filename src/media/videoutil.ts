9937e839b15aa34f2f82e309b5b8fa6941bf928e
Rafi Khan <rafikhan@gmail.com>
2023-12-07 08:57:19 -0800

    Attemping to make video playback more reliable (fingers crossed)

--- src/media/videoutil.ts
+++ src/media/videoutil.ts
@@ -1,14 +1,34 @@
 export function playAndWait(videoElt: HTMLVideoElement) {
-    return new Promise<void>((resolve) => {
-        if (videoElt.paused) {
+    return new Promise<void>((resolve, reject) => {
+        if (!isPlaying(videoElt)) {
             console.info('Play');
-            videoElt.addEventListener('play', () => resolve(), { once: true });
-            videoElt.play();
+
+            // See https://stackoverflow.com/a/37172024 for why this timeout is used.
+            setTimeout(() => {
+                videoElt.play().then(resolve).catch(reject);
+            }, 10);
         } else {
             console.info('Play (no-op)');
             resolve();
         }
     });
+}
+
+/**
+ * Helper function used to help reduce the risk of "The play() request was
+ * interrupted by a call to pause()" exception.
+ *
+ * For context see:
+ *  - https://stackoverflow.com/questions/36803176/how-to-prevent-the-play-request-was-interrupted-by-a-call-to-pause-error
+ *  - https://developer.chrome.com/blog/play-request-was-interrupted
+ */
+function isPlaying(videoElt: HTMLVideoElement) {
+    return (
+        videoElt.currentTime > 0 &&
+        !videoElt.paused &&
+        !videoElt.ended &&
+        videoElt.readyState > videoElt.HAVE_CURRENT_DATA
+    );
 }
 
 export function pauseAndWait(videoElt: HTMLVideoElement) {
