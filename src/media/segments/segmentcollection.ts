62864bb70a497ba1cae01e4cbdfe4c798bce850d
Rafi Khan <rafikhan@gmail.com>
2023-12-08 21:09:36 -0800

    Switching to logger on the segment collection

--- src/media/segments/segmentcollection.ts
+++ src/media/segments/segmentcollection.ts
@@ -1,10 +1,18 @@
 import EventEmitter from 'events';
 import { Segment } from '~/media/interfaces';
+import { Logger, getLog } from '../logutil';
 import { formatSegment, formaSegmentSpan as formatSegmentSpan } from './formatutil';
 
 export class SegmentCollection extends EventEmitter {
+    private logger: Logger;
     private _recordingStartTime: Date | null = null;
     private readonly _segments: Segment[] = [];
+
+    constructor() {
+        super();
+
+        this.logger = getLog('seg-coll', { logging: 'info' });
+    }
 
     get hasRecordingStartTime() {
         return !!this._recordingStartTime;
@@ -49,7 +57,7 @@ export class SegmentCollection extends EventEmitter {
     }
 
     addSegment(segment: Segment) {
-        this.log(`Adding ${formatSegment(segment)}`);
+        this.logger.log(`Adding ${formatSegment(segment)}`);
 
         this._segments.push(segment);
         this.cleanAllStartTimes();
@@ -62,16 +70,16 @@ export class SegmentCollection extends EventEmitter {
 
         if (timestamp <= 0) {
             const segment = segments[0];
-            this.log(`Min ${formatSegmentSpan(segment, timestamp)}`);
+            this.logger.log(`Min ${formatSegmentSpan(segment, timestamp)}`);
             return { segment, offset: 0 };
         } else if (timestamp >= this.duration) {
             const segment = segments[segments.length - 1];
-            this.log(`Max ${formatSegmentSpan(segment, timestamp)}`);
+            this.logger.log(`Max ${formatSegmentSpan(segment, timestamp)}`);
             return { segment, offset: segment.startTime + segment.duration };
         } else {
             const segment = this.findClosestSegmentForTimestamp(timestamp);
 
-            this.log(`Mid ${formatSegmentSpan(segment, timestamp)}`);
+            this.logger.log(`Mid ${formatSegmentSpan(segment, timestamp)}`);
 
             const offset = Math.max(0, timestamp - segment.startTime);
 
@@ -142,7 +150,9 @@ export class SegmentCollection extends EventEmitter {
             return false;
         }
 
-        this.log(`Resetting duration of ${formatSegment(segment)} to ${duration.toFixed(2)}`);
+        this.logger.log(
+            `Resetting duration of ${formatSegment(segment)} to ${duration.toFixed(2)}`
+        );
 
         segment.duration = duration;
 
@@ -175,9 +185,5 @@ export class SegmentCollection extends EventEmitter {
 
     private emitDurationChange(segment: Segment) {
         this.emit('durationchange', segment);
-    }
-
-    private log(message: string) {
-        // console.log(message);
     }
 }
