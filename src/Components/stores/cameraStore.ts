47477123eeb4902ed2dcf2e59203bb3fa30ef409
Rafi Khan <rafikhan@gmail.com>
2023-12-09 10:27:06 -0800

    Don't need the camera device id constants anymore

--- src/Components/stores/cameraStore.ts
+++ src/Components/stores/cameraStore.ts
@@ -1,5 +1,9 @@
 import { makeAutoObservable, observable } from 'mobx';
 
+// export const LOGITECH_BRIO_CAMERA_ID =
+//     '94f3aff55f18fcaa8238d5ae2437608852fcdeae132d61a15b94f197cf364acb';
+// export const BUILT_IN = '5134f09eebf96f0a8bc51de97e5b2bfb78e846b2cb5791c35516010b8350fc18';
+
 export class CameraStore {
     constructor() {
         makeAutoObservable<CameraStore, '_lastSelectedCameraId'>(this, {
