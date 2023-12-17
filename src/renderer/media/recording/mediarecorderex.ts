6a5149a72267fc11f7b74ded9e57d9d4751432de
Rafi Khan <rafikhan@gmail.com>
2023-12-17 10:14:02 -0800

    Pulling in mediarecorderex.ts

---
+++ src/renderer/media/recording/mediarecorderex.ts
@@ -0,0 +1,59 @@
+import dayjs, { Dayjs } from 'dayjs';
+import EventEmitter from 'events';
+import _merge from 'lodash.merge';
+import { RecordingOptions } from '~/renderer/media';
+import { DEFAULT_RECORDING_OPTIONS } from '~/renderer/media/constants';
+import { Logger, getLog } from '~/renderer/media/logutil';
+import TypedEventEmitter from '../eventemitter';
+
+type MediaRecorderExEvents = {
+    recordingerror: (error: any) => void;
+    start: (startTime: Dayjs, blob: Blob) => void;
+    append: (blob: Blob) => void;
+};
+
+export class MediaRecorderEx extends (EventEmitter as new () => TypedEventEmitter<MediaRecorderExEvents>) {
+    private logger: Logger;
+    private readonly recorder: MediaRecorder;
+    public readonly options: RecordingOptions;
+
+    constructor(private readonly stream: MediaStream, options?: Partial<RecordingOptions>) {
+        super();
+
+        this.options = _merge({}, DEFAULT_RECORDING_OPTIONS, options);
+        this.logger = getLog('seg-rec', this.options);
+
+        this.recorder = new MediaRecorder(this.stream, {
+            mimeType: this.options.mimeType,
+        });
+        this.recorder.ondataavailable = this.onDataAvailable;
+    }
+
+    private _startTime!: dayjs.Dayjs;
+
+    start() {
+        try {
+            this.isFirstChunk = true;
+
+            this.recorder.start(this.options.minSegmentSizeInSec * 1000);
+            this._startTime = dayjs();
+        } catch (err) {
+            this.emit('recordingerror', err);
+        }
+    }
+
+    async stop() {
+        this.recorder.stop();
+    }
+
+    private isFirstChunk = true;
+
+    private onDataAvailable = (event: BlobEvent) => {
+        if (this.isFirstChunk) {
+            this.emit('start', this._startTime, event.data);
+            this.isFirstChunk = false;
+        } else {
+            this.emit('append', event.data);
+        }
+    };
+}
