122a71449b6892d93eaeba2466f13ee1f5b80775
Rafi Khan <rafikhan@gmail.com>
2023-12-17 10:14:19 -0800

    Pulling in window.listoApi definition

--- src/renderer/media/extensions.d.ts
+++ src/renderer/media/extensions.d.ts
@@ -1,3 +1,4 @@
+import { LISTO_API } from '~/preload/listoApi';
 import { DvrStore } from '~/renderer/Components/stores/dvrStore';
 import { DigitalVideoRecorder } from './dvr';
 
@@ -5,5 +6,6 @@ declare global {
     interface Window {
         dvr: DigitalVideoRecorder;
         dvrStore: DvrStore;
+        listoApi: typeof LISTO_API;
     }
 }
