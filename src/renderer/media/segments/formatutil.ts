3a44421dc848da777fb4df357234077b04e49cac
Rafi Khan <rafikhan@gmail.com>
2023-12-19 21:33:55 -0800

    switching timestamp -> timecode

--- src/renderer/media/segments/formatutil.ts
+++ src/renderer/media/segments/formatutil.ts
@@ -4,8 +4,8 @@ export function formatSegment({ index, startTime, duration }: Segment) {
     return `segment=${index}, start=${startTime.toFixed(2)}, duration=${duration.toFixed(2)}`;
 }
 
-export function formaSegmentSpan(segment: Segment, timestamp: number) {
-    return `${segment.index} = ${segment.startTime} <= ${timestamp} < ${
+export function formaSegmentSpan(segment: Segment, timecode: number) {
+    return `${segment.index} = ${segment.startTime} <= ${timecode} < ${
         segment.startTime + segment.duration
     }`;
 }
