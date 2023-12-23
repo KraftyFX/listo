66fc64897303855484be2dc6ed95ffd34d3af8ec
Rafi Khan <rafikhan@gmail.com>
2023-12-22 19:24:57 -0800

    Removing dead code

--- src/renderer/media/segments/formatutil.ts
+++ src/renderer/media/segments/formatutil.ts
@@ -3,10 +3,3 @@ import { Segment } from './interfaces';
 export function formatSegment({ index, startOffset, duration }: Segment) {
     return `segment=${index}, start=${startOffset.toFixed(2)}, duration=${duration.toFixed(2)}`;
 }
-
-export function formaSegmentSpan(segment: Segment, timecode: number) {
-    const start = segment.startOffset.toFixed(2);
-    const end = (segment.startOffset + segment.duration).toFixed(2);
-
-    return `${segment.index} = ${start} <= ${timecode} < ${end}`;
-}
