176c3f3705fe94f758d8f6b9847d7bdf315e6082
Rafi Khan <rafikhan@gmail.com>
2023-12-23 10:22:50 -0800

    Adding more dayjs util functions

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,10 +1,16 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
+import isBetween from 'dayjs/plugin/isBetween';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ListoApp } from '~/renderer/Components/ListoApp';
 
 dayjs.extend(duration);
+dayjs.extend(isBetween);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
 
 export async function initListo(rootElt: HTMLElement) {
     const root = createRoot(rootElt);
