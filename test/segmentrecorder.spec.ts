01f3fdf14bf9ff963b50e15ca9fa61513134e494
Rafi Khan <rafikhan@gmail.com>
2023-12-24 14:43:51 -0800

    Removing flaky test

--- test/segmentrecorder.spec.ts
+++ test/segmentrecorder.spec.ts
@@ -17,45 +17,22 @@ describe('SegmentRecorder', function () {
 
     after(async () => {});
 
-    it('Can record 2 seconds of video', async () => {
-        const recorder = new SegmentRecorder(stream, { inMemory: true, minSegmentSizeInSec: 1 });
-
-        let count = 0;
-
-        recorder.onrecording = async () => {
-            count++;
-
-            if (count > 2) {
-                assert.fail(`Recording happened when it should not have`);
-            }
-        };
-
-        try {
-            recorder.startRecording();
-
-            await waitFor(4000);
-
-            assert.equal(count, 2, `count`);
-        } finally {
-            await recorder.stopRecording();
-        }
-    });
-
-    it(`Cannot record video after being stopped`, async () => {
+    it(`Does not record video after being stopped`, async () => {
         const recorder = new SegmentRecorder(stream, { inMemory: true, minSegmentSizeInSec: 2 });
-
-        let count = 0;
 
         recorder.onrecording = async () => {
             assert.fail(`Recording happened when it should not have`);
         };
 
         recorder.startRecording();
+        assert.isTrue(recorder.isRecording, `isRecording`);
+
+        await waitFor(1000);
         await recorder.stopRecording();
 
         await waitFor(1000);
 
-        assert.equal(count, 0, `count`);
+        assert.isFalse(recorder.isRecording, `isRecording`);
     });
 });
 
