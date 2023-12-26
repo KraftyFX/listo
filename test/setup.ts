e8475796debc263518d6f0cd6e213b29f7d2f61e
Rafi Khan <rafikhan@gmail.com>
2023-12-26 15:58:28 -0800

    Rigging test setup

---
+++ test/setup.ts
@@ -0,0 +1,17 @@
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import isBetween from 'dayjs/plugin/isBetween';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import { ServiceLocator, setLocator } from '~/renderer/services';
+import { MockStreamRecorder } from './services/streamrecorder.mock';
+import { MockVideoPlayer } from './services/videoplayer.mock';
+
+dayjs.extend(duration);
+dayjs.extend(isBetween);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
+
+before(() => {
+    setLocator(new ServiceLocator(new MockVideoPlayer(), new MockStreamRecorder()));
+});
