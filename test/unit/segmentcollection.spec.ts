7d17b33e312baf596bee38c9d82eb2d4003af642
Rafi Khan <rafikhan@gmail.com>
2024-01-01 18:22:27 -0800

    Adding lots more tests to segment collection

--- test/unit/segmentcollection.spec.ts
+++ test/unit/segmentcollection.spec.ts
@@ -1,78 +1,243 @@
 import { assert } from 'chai';
-import dayjs from 'dayjs';
 import { Segment } from '~/renderer/media/segments/interfaces';
 import { SegmentCollection } from '~/renderer/media/segments/segmentcollection';
+import { getLocator } from '~/renderer/services';
 
 describe.only('SegmentCollection', () => {
-    it('can add a segment', async () => {
-        const segments = new SegmentCollection();
+    describe('addSegment()', () => {
+        it('once', async () => {
+            const { host } = getLocator();
+            const segments = new SegmentCollection();
 
-        let raised: Segment = null!;
+            let raised: Segment = null!;
 
-        segments.on('segmentadded', (segment) => {
-            raised = segment;
-        });
+            segments.on('segmentadded', (segment) => {
+                raised = segment;
+            });
 
-        const returned = segments.addSegment(dayjs(), 'test://', 5, false);
-
-        assert.isNotNull(raised, 'raised');
-        assert.isNotNull(returned, 'returned');
-
-        assert.equal(raised, returned, 'raised and returned');
-
-        assert.equal(segments.length, 1, 'count');
-    });
-
-    it('can add a few segments', async () => {
-        const segments = new SegmentCollection();
-        const start = dayjs();
-
-        let raised: Segment = null!;
-
-        segments.on('segmentadded', (segment) => {
-            raised = segment;
-        });
-
-        let last: Segment = null!;
-
-        for (let i = 0; i < 3; i++) {
-            last = segments.addSegment(start.add(5 * i, 'seconds'), 'test://', 5, false);
+            const returned = segments.addSegment(host.now, 'test://', 5, false);
 
             assert.isNotNull(raised, 'raised');
-            assert.isNotNull(last, 'returned');
+            assert.isNotNull(returned, 'returned');
 
-            assert.equal(raised, last, 'raised and returned');
+            assert.equal(raised, returned, 'raised and returned');
 
-            assert.equal(segments.length, i + 1, 'count');
-            assert.equal(segments.lastSegment, last, `last segment at ${i}`);
-        }
+            assert.equal(segments.length, 1, 'count');
+        });
+
+        it('many times', async () => {
+            const { host } = getLocator();
+            const segments = new SegmentCollection();
+
+            let raised: Segment = null!;
+
+            segments.on('segmentadded', (segment) => {
+                raised = segment;
+            });
+
+            let last: Segment = null!;
+
+            for (let i = 0; i < 3; i++) {
+                last = segments.addSegment(host.now, 'test://', 5, false);
+
+                assert.isNotNull(raised, 'raised');
+                assert.isNotNull(last, 'returned');
+
+                assert.equal(raised, last, 'raised and returned');
+
+                assert.equal(segments.length, i + 1, 'count');
+                assert.equal(segments.lastSegment, last, `last segment at ${i}`);
+
+                host.advanceTimeBy(5000);
+            }
+        });
+
+        it('many times with last partial', async () => {
+            const { host } = getLocator();
+            const segments = getWithDenseSegments();
+
+            segments.addSegment(host.now, 'test://', 5, true);
+
+            assert.isTrue(segments.isLastSegmentPartial, 'partial');
+        });
+
+        it('replaces last partial with new partial', async () => {
+            const { host } = getLocator();
+            const segments = new SegmentCollection();
+
+            const partial = segments.addSegment(host.now, 'test://', 3, true);
+            host.advanceTimeBy(3000);
+
+            assert.isTrue(segments.isLastSegmentPartial, 'isLastSegmentPartial');
+
+            // Fake dispose
+            partial.url = '';
+
+            const replacement = segments.addSegment(host.now, 'test://', 4, true);
+            host.advanceTimeBy(4000);
+
+            assert.isTrue(segments.isLastSegmentPartial, 'isLastSegmentPartial');
+
+            assert.equal(segments.lastSegment, replacement, 'last segment');
+            assert.equal(segments.length, 1, 'count');
+        });
+
+        it('replaces last partial with new full partial', async () => {
+            const { host } = getLocator();
+            const segments = new SegmentCollection();
+
+            const partial = segments.addSegment(host.now, 'test://', 3, true);
+            host.advanceTimeBy(3000);
+
+            assert.isTrue(segments.isLastSegmentPartial, 'isLastSegmentPartial');
+
+            // Fake dispose
+            partial.url = '';
+
+            const replacement = segments.addSegment(host.now, 'test://', 4, false);
+            host.advanceTimeBy(4000);
+
+            assert.isFalse(segments.isLastSegmentPartial, 'isLastSegmentPartial');
+            assert.equal(segments.lastSegment, replacement, 'last segment');
+            assert.equal(segments.length, 1, 'count');
+        });
     });
 
-    it('can replace a partial segment with a new partial segment', async () => {
-        const segments = new SegmentCollection();
+    describe('Properties', () => {
+        it('firstSegmentStartTime is correct', async () => {
+            const segments = getWithDenseSegments();
 
-        const partial = segments.addSegment(dayjs(), 'test://', 3, true);
+            assert.equal(
+                segments.firstSegmentStartTime,
+                segments.segments[0].startTime,
+                'start times'
+            );
+        });
 
-        // Fake dispose
-        partial.url = '';
+        it('lastSegment is correct', async () => {
+            const segments = getWithDenseSegments();
+            const actual = segments.segments[segments.length - 1];
+            const expected = segments.lastSegment;
 
-        const replacement = segments.addSegment(dayjs().add(3, 'seconds'), 'test://', 4, true);
+            assert.equal(actual, expected, 'segment');
+        });
 
-        assert.equal(segments.lastSegment, replacement, 'last segment');
-        assert.equal(segments.length, 1, 'count');
+        it('lastSegmentEndTime is correct', async () => {
+            const segments = getWithDenseSegments();
+            const { startTime, duration } = segments.segments[segments.length - 1];
+
+            const actual = startTime.add(duration, 'seconds');
+            const expected = segments.lastSegmentEndTime;
+
+            assert.equal(actual.valueOf(), expected.valueOf(), 'segment');
+        });
     });
 
-    it('can replace a partial segment with a new full segment', async () => {
-        const segments = new SegmentCollection();
+    describe('Segment instance tests', () => {
+        it('isFirstSegment(first) succeeds', async () => {
+            const segments = getWithDenseSegments();
+            const segment = segments.segments[0];
 
-        const partial = segments.addSegment(dayjs(), 'test://', 3, true);
+            assert.isTrue(segments.isFirstSegment(segment), 'first segment');
+        });
 
-        // Fake dispose
-        partial.url = '';
+        it('isFirstSegment(other) fails', async () => {
+            const segments = getWithDenseSegments();
+            const segment = segments.segments[1];
 
-        const replacement = segments.addSegment(dayjs().add(3, 'seconds'), 'test://', 4, false);
+            assert.isFalse(segments.isFirstSegment(segment), 'first segment');
+        });
 
-        assert.equal(segments.lastSegment, replacement, 'last segment');
-        assert.equal(segments.length, 1, 'count');
+        it('isLastSegment(last) succeeds', async () => {
+            const segments = getWithDenseSegments();
+            const segment = segments.lastSegment;
+
+            assert.isTrue(segments.isLastSegment(segment), 'first segment');
+        });
+
+        it('isLastSegment(other) fails', async () => {
+            const segments = getWithDenseSegments();
+            const segment = segments.segments[1];
+
+            assert.isFalse(segments.isLastSegment(segment), 'first segment');
+        });
+
+        it('getNextSegment(first) is correct', async () => {
+            const segments = getWithDenseSegments();
+            const curr = segments.segments[0];
+            const next = segments.segments[1];
+
+            assert.equal(segments.getNextSegment(curr), next, 'segment');
+        });
+
+        it('getNextSegment(mid) is correct', async () => {
+            const segments = getWithDenseSegments();
+            const curr = segments.segments[2];
+            const next = segments.segments[3];
+
+            assert.equal(segments.getNextSegment(curr), next, 'segment');
+        });
+
+        it('getNextSegment(last) is null', async () => {
+            const segments = getWithDenseSegments();
+            const curr = segments.lastSegment;
+            const next = null;
+
+            assert.equal(segments.getNextSegment(curr), next, 'segment');
+        });
+
+        it('getNextSegment(null) is null', async () => {
+            const { host } = getLocator();
+            const segments = new SegmentCollection();
+            const curr = segments.addSegment(host.now, 'test://', 0, false);
+            const next = null;
+
+            assert.equal(segments.getNextSegment(curr), next, 'segment');
+        });
+    });
+
+    describe('containsTime()', () => {
+        it('before firstSegmentStartTime fails', async () => {
+            const segments = getWithDenseSegments();
+            const time = segments.firstSegmentStartTime.subtract(1, 'second');
+
+            assert.isFalse(segments.containsTime(time), 'time');
+        });
+
+        it('within first/last segment start/end time succeeds', async () => {
+            const segments = getWithDenseSegments();
+            const duration = segments.lastSegmentEndTime.diff(segments.firstSegmentStartTime);
+            const time = segments.firstSegmentStartTime.add(duration / 2, 'milliseconds');
+
+            assert.isTrue(segments.containsTime(time), 'time');
+        });
+
+        it('after lastSegmentEndTime fails', async () => {
+            const segments = getWithDenseSegments();
+            const time = segments.lastSegmentEndTime.add(1, 'second');
+
+            assert.isFalse(segments.containsTime(time), 'time');
+        });
     });
 });
+
+function getWithDenseSegments() {
+    const segments = new SegmentCollection();
+    const { host } = getLocator();
+    let now = host.now;
+
+    for (let i = 0; i < 5; i++) {
+        // Durations are usually +/0 0.1 sec of the desired time
+        const duration = 4.9 + 0.2 * Math.random();
+
+        segments.addSegment(now, 'test://', duration, false);
+
+        // Start times are pretty much never the previous start time + duration.
+        // there's always data loss caused by computational overhead.
+        now = now.add(duration + 0.001, 'seconds');
+
+        host.advanceTimeBy((duration + 0.001) * 1000);
+    }
+
+    return segments;
+}
